72e0680215009987c1955874c8be501444b7277c
Win Lwin <winlwinlwintun@gmail.com>
2020-05-29 23:58:35 +0630

    Add Delete Button & active/deactive

--- public/js/admins/assistant.js
+++ public/js/admins/assistant.js
@@ -36,9 +36,11 @@ $(document).ready(() => {
                 "data": null,
                 "render": function(data,type,row) {
                     if(data['active'] === true) {
-                        return `<span class='badge badge-success'>Active</span>`
+                        return '<span class="badge badge-success mr-2 dropdown-toggle" id="dropdownmenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Active</span>'+
+                        '<div class="dropdown-menu" aria-labelledby="dropdownmune"><span class="disablebtn dropdown-item">Disable</span></div>';
                     } else {
-                        return `<span class='badge badge-danger'>Inactive</span>`
+                        return '<span class="badge badge-warning mr-2 dropdown-toggle" id="dropdownmenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Disable</span>'+
+                        '<div class="dropdown-menu" aria-labelledby="dropdownmune"><span class="activebtn dropdown-item">Active</span></div>';
                     }
                 }
             },
@@ -47,10 +49,23 @@ $(document).ready(() => {
                 "render": function(data,type,row) {
                     let date = new Date(data['createdAt']);
                     let day = date.getFullYear()+'.'+(date.getMonth()+1)+'.'+date.getDate();
-                    let time = date.getHours()+':'+(date.getMinutes()+1)+':'+date.getSeconds();
-                    let fulldate = day+' '+time;
+                    let hours = date.getHours();
+                    let minutes = (date.getMinutes()+1);
+                    let seconds = date.getSeconds();
+                    let ampm = hours >= 12 ? 'PM' : 'AM';
+                    hours = hours % 12;
+                    hours = hours ? hours : 12; 
+                    minutes = minutes < 10 ? '0'+minutes : minutes;
+                    seconds = seconds < 10 ? '0'+seconds : seconds;
+                    let time = hours+':'+minutes+':'+seconds+' '+ampm;
+                    let fulldate = day+'<br>'+time;
                     return fulldate;
                 }
+            },
+            { "data" : null,
+              "render": function(data,type,row) {
+                return `<span class='btn btn-danger custom-action-btn'><i class='fas fa-trash'>Delete</i></span>`
+              }
             },
 
         ],
@@ -91,5 +106,78 @@ $(document).ready(() => {
                 $('#errorModalTitle').html('Process Failed');
                 $('#errorModalContent').html('Your process of adding assistant is failed. Please try again.');
             }); 
+    })
+    /**
+     * Disable Button
+     */
+    dataTable.on('click', 'tbody > tr > td > div > .disablebtn', function (e) {
+        e.preventDefault();
+
+        var tr = $(this).closest('tr');
+        var row = dataTable.row( tr );
+        let data = row.data();
+        
+        const id = data._id;
+        const active = false;
+        //alert (id);
+        axios.put(`/api/admin/assistants/${id}`,{active}, {headers})
+        .then(response => {
+            dataTable.ajax.reload();
+        })
+    });
+
+    /**
+     * Active Button
+     */
+    dataTable.on('click', 'tbody > tr > td > div > .activebtn', function (e) {
+        e.preventDefault();
+
+        var tr = $(this).closest('tr');
+        var row = dataTable.row( tr );
+        let data = row.data();
+        
+        const id = data._id;
+        const active = true;
+        axios.put(`/api/admin/assistants/${id}`,{active}, {headers})
+        .then(response => {
+            dataTable.ajax.reload();
+        })
+    });
+
+    //remove assistant
+    dataTable.on('click', 'tbody > tr > td > .btn-danger', function(e) {
+        e.preventDefault();
+        var tr = $(this).closest('tr');
+        var row = dataTable.row( tr );
+        let data = row.data();
+
+        currentData = data;
+        $('#assistantDeleteModal').modal('show');
+    })
+
+    //remove assistant form
+    $('#assistantDelForm').submit(e => {
+        e.preventDefault();
+
+        const confirm = $('#confirmDel').val();
+
+        if(confirm.toLowerCase() === 'confirm') {
+            axios.delete(`/api/admin/assistants/${currentData._id}`,{headers})
+                .then(response => {
+                    $('#assistantDeleteModal').modal('hide');
+
+                    $('#successModal').modal('show');
+                    $('#successModalTitle').html('Success');
+                    $('#successModalContent').html('A machine is deleted successfully.');
+
+                    dataTable.ajax.reload();
+                }).catch(e => {
+                    $('#assistantDeleteModal').modal('hide');
+                    
+                    $('#errorModal').modal('show');
+                    $('#errorModalTitle').html('Deleting Machine Failed');
+                    $('#errorModalContent').html('The process of deleting machine is failed. Please check your data and retry again.');
+                })
+        }
     })
 })
